363fdad1b73ae0406af153f6ef0d9a214b8806e1
mangena-dave <david@rapidtrade.biz>
2017-01-13 11:09:36 +0200

    converting coffee script to javaScript

--- RapidTargets/basics.js
+++ RapidTargets/basics.js
@@ -1,5 +1,4 @@
-// mocha basics.js --timeout 6000000
-
+// mocha basics.js --timeout 30000
 var chai, deselectDF, expect, gocustomer, newcustomer, screenshot, selectDF, selenium, waitFor;
 
 selenium = require("selenium-webdriver");
@@ -13,7 +12,7 @@ expect = chai.expect;
 before(function() {
   this.timeout = 8000;
   global.driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
-  global.url = "http://localhost/git/rapidtargets";
+  global.url = "http://betanew.rapidtrade.mobi/";
   global.timeout = 30000;
   driver.getWindowHandle();
   driver.get(url);
@@ -228,25 +227,22 @@ describe.only("Activities", function() {
     driver.findElement({xpath: "//a[p='First Meeting'][1]"}).click();
     driver.sleep(500);
     driver.findElement({id: "note"}).sendKeys("Looks like a good company");
-    driver.get(url + "/#/opportunity/new")
-    //driver.findElement({id: "newBtn"}).click();
+    //driver.get(url + "/#/opportunity/new")
+    driver.findElement({id: "newBtn"}).click();
     driver.sleep(500);
     driver.findElement({id: "upfrontFee"}).clear();
     driver.findElement({id: "monthlyFee"}).clear();
     driver.findElement({id: "closeByDate"}).clear();
-    driver.findElement({xpath: "//div[@class='form-group'][2]/div[@class='col-sm-10']/input[@id='name']"}).sendKeys("Rapid Sales");
-    driver.findElement({xpath: "//div[@class='form-group'][3]/div[@class='col-sm-10']/input[@id='description']"}).sendKeys("Rapidsales opportunity");
-    driver.findElement({xpath: "//div[@class='form-group'][4]/div[@class='col-sm-10 input-group']/input[@id='upfrontFee']"}).sendKeys("1000");
-    driver.findElement({xpath: "//div[@class='form-group'][6]/div[@class='col-sm-10 input-group']/input[@id='monthlyFee']"}).sendKeys("100");
-    driver.findElement({xpath: "//div[@class='form-group'][7]/div[@class='col-sm-10']/input[@id='closeByDate']"}).sendKeys("January 1, 2018");
+    driver.findElement({id: "name"}).sendKeys("Rapid Sales");
+    driver.findElement({id: "description"}).sendKeys("Rapidsales opportunity");
+    driver.findElement({id: "upfrontFee"}).sendKeys("1000");
+    driver.findElement({id: "monthlyFee"}).sendKeys("100");
+    driver.findElement({id: "closeByDate"}).sendKeys("January 1, 2018");
     driver.findElement({linkText: "Save"}).click();
     return waitFor({xpath: "//div[@class='alert ng-scope top-right am-fade alert-success']"}, "\nCould not create opportunity");
   });
   it("Check activity details are remembered and save the activity", function() {
     driver.sleep(4000);
-    driver.findElement({linkText: "Back"}).click();
-    driver.sleep(500)
-    //waitFor({id: "Opportunity"}, "No opportunity input field");
     driver.findElement({id: "Opportunity"}).getAttribute("value").then(function(opp) {
       return expect(opp).to.not.equal("");
     });
